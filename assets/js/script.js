cd5901e11b98adf24de2700ffbf67668b61d403d
Brian Bellanger <brianjbellanger@gmail.com>
2021-04-06 12:06:17 -0500

    BriTueApr6-MusixMatch

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,45 @@ var searchbutton = document.querySelector('.is-info')
 var input = document.querySelector('.input')
 const youtubekey = 'AIzaSyDGxdfjPLDMkjD0Cvi9dU8d66Pv1SlJ08k'
 
+// MusixMatch 'UT Student's' appid	6a4d09aa7c7bc21dd8f981caaf324cda
+// DOM Vars
+var searchBtn = document.getElementById("button-addon");
+var songInputEl = document.querySelector(".musicForm");
+
+// Global Vars
+
+
+// Functions
+var submitForm = function (event) {
+    event.preventDefault();
+    var songSearch = songInputEl.value.trim();
+    if (songSearch) {
+        getSongs(songSearch);
+        songInputEl.value = '';
+    } else {
+        alert('Please enter the name of a song');
+    }
+};
+
+function getSongs (songTitle) {
+    var apiUrl = 'https://api.musixmatch.com/ws/1.1/track.search?q_track=' + songTitle + '&page_size=5&apikey=6a4d09aa7c7bc21dd8f981caaf324cda';
+    fetch(apiUrl).then (function(response){
+            if (response.ok){
+                response.json().then(function(currentData){
+                    console.log(currentData);
+                    // displaySongs(currentData, songTitle);
+                    // getLyrics(currentData, songTitle);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to MusiXmatch');
+        });
+};
+
+
 //onclick search button consol logs in the input box with place holder 'Search Song'
 searchbutton.addEventListener('click', function (event) {
     console.log(input.value)
