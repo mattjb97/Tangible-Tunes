80bba70c712c8d618570ace02c17459b2b089b75
Mattjb97 <bohlmatthew@gmail.com>
2021-04-06 14:25:30 -0500

    function to make youtube videos populate on click of the search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,45 @@
 var searchbutton = document.querySelector('.is-info')
-var input = document.querySelector('.input')
-const youtubekey = 'AIzaSyDGxdfjPLDMkjD0Cvi9dU8d66Pv1SlJ08k'
+var input = document.querySelector('#search')
+
 
 //onclick search button consol logs in the input box with place holder 'Search Song'
 searchbutton.addEventListener('click', function (event) {
     console.log(input.value)
-
+    event.preventDefault()
 });
 
 
-'https://www.googleapis.com/youtube/v3/videos'
 
-function getMusic(Song) {
-    $.ajax({
-        type: 'GET',
-        url: 'https://www.googleapis.com/youtube/v3/search',
-        data: {
-            key: youtubekey,
-            q: Song + " Music",
-            part: 'snippet',
-            maxResults: 1,
-            type: 'video',
-            videoEmbeddable: true
-        },
+$(document).ready(function () {
+    var youtubekey = 'AIzaSyDGxdfjPLDMkjD0Cvi9dU8d66Pv1SlJ08k'
+    var video = ''
+
+    $('#form').on('click', function (event) {
+        event.preventDefault()
+        console.log('clicked')
+        var search = $('#search').val()
+        videoSearch(youtubekey, search, 5)
+        console.log(search.value)
+     
     })
-}
+    
+    function videoSearch(key, search, maxResults) {
+        $('#videos').empty()
+        $.get('https://www.googleapis.com/youtube/v3/search?key=' + key +
+            '&type=video&part=snippet&maxResults=' + maxResults + '&q=' + search, function (data) {
+                console.log(data)
+
+                data.items.forEach(item => {
+                    video = `
+                
+                <iframe class="has-ratio" width="640" height="360" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" 
+                allowfullscreen></iframe>
+                
+                `
+                    $('#videos').append(video)
+                });
+            })
+    }
+    
+})
 
