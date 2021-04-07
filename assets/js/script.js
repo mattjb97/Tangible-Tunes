fc8b4a219976b3551138ce592c25bf5a9265935d
Mattjb97 <bohlmatthew@gmail.com>
2021-04-07 12:03:15 -0500

    clean up and brainstorm on function fixing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,8 @@ var searchbutton = document.querySelector('.is-info')
 var input = document.querySelector('#search')
 
 
-var firstSong = document.querySelector('.firstSong')
-var secondSong = document.querySelector('.secondSong')
-var thirdSong = document.querySelector('.thirdSong')
-var fourthSong = document.querySelector('.fourthSong')
-var fifthSong = document.querySelector('.fifthSong')
 var videoLocation = document.querySelector('.boxVideo')
 var lyricsLocation = document.querySelector('.boxLyrics')
-
-
 
 
 
@@ -21,80 +14,94 @@ var lyricsLocation = document.querySelector('.boxLyrics')
 
 
 
-// searchbutton.addEventListener('click', function (event) {
-//     console.log(input.value)
-//     event.preventDefault();
-// });
+searchbutton.addEventListener('click', function (event) {
+    console.log(input.value)
+    event.preventDefault();
+});
 
 
-// $(document).ready(function () {
-//     $('#form').on('click', function (event) {
-//         event.preventDefault()
-//         var search = $('#search').val()
-//         getSongs(search)
-//     })
+$(document).ready(function () {
+    $('#form').on('click', function (event) {
+        event.preventDefault()
+        var search = $('#search').val()
+        getSongs(search)
+    })
 
-//     function getSongs(songTitle) {
-//         var apiUrl = 'https://api.musixmatch.com/ws/1.1/track.search?q_track=' + songTitle + '&page_size=5&s_track_rating=desc&apikey=6a4d09aa7c7bc21dd8f981caaf324cda';
-//         fetch(apiUrl).then(function (response) {
-//             if (response.ok) {
-//                 response.json().then(function (currentData) {
-//                     console.log(currentData);
-//                     var songList = currentData.message.body.track_list;
-//                     console.log(songList);
-//                     displayList(songList);
-//                 });
-//             } else {
-//                 alert('Error: ' + response.statusText);
-//             }
-//         })
-//             .catch(function (error) {
-//                 alert('Unable to connect to MusiXmatch');
-//             });
-//     };
+    function getSongs(songTitle) {
+        var apiUrl = 'https://api.musixmatch.com/ws/1.1/track.search?q_track=' + songTitle + '&page_size=5&s_track_rating=desc&apikey=6a4d09aa7c7bc21dd8f981caaf324cda';
+        fetch(apiUrl).then(function (response) {
+            if (response.ok) {
+                response.json().then(function (currentData) {
+                    console.log(currentData);
+                    var songList = currentData.message.body.track_list;
+                    console.log(songList);
+                    displayList(songList);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+            .catch(function (error) {
+                alert('Unable to connect to MusiXmatch');
+            });
+    };
+});
+function displayList(songArray) {
+    var appendEl = document.querySelector(".list-group");
+    appendEl.innerHTML = "";
+    for (var i = 0; i < songArray.length; i++) {
+        var appendEl = document.querySelector(".list-group");
+        var liEl = document.createElement("li");
+        liEl.textContent = "Artist: " + songArray[i].track.artist_name + " - Song: " + songArray[i].track.track_name;
+        liEl.classList.add("list-group-item");
+        $('.list-group').append(liEl)[i]
+        var trackID = (songArray[i].track.track_id)
+        console.log(trackID)
+        console.log(songArray[i].track.track_name + songArray[i].track.artist_name)
+        }
 
-//     function displayList(songArray) {
-//         var appendEl = document.querySelector(".list-group");
-//         appendEl.value = "";
-//         for (var i = 0; i < songArray.length; i++) {
-//             var appendEl = document.querySelector(".list-group");
-//             var liEl = document.createElement("li");
-//             liEl.textContent = "Artist: " + songArray[i].track.artist_name + " - Song: " + songArray[i].track.track_name;
-//             liEl.classList.add("list-group-item");
-//             $('.list-group').append(liEl)[i]
-           
-
-//         }
-
-        
-//     }
-// });
 
 
 
 $(document).ready(function () {
-    var youtubekey = 'AIzaSyDGxdfjPLDMkjD0Cvi9dU8d66Pv1SlJ08k'
-    var video = ''
-    $('#form').on('click', function (event) {
+    $('.list-group').on('click', function (event) {
+        var listEl = document.getElementsByClassName("list-group-item");
+        console.log(listEl)
         event.preventDefault()
-        console.log('clicked')
-        var search = $('#search').val()
-        videoSearch(youtubekey, search, 5)
-        console.log(search.value)
+        
+
+
     })
-    function videoSearch(key, search, maxResults) {
-        $('#videos').empty()
-        $.get('https://www.googleapis.com/youtube/v3/search?key=' + key +
-            '&type=video&part=snippet&maxResults=' + maxResults + '&q=' + search, function (data) {
-                console.log(data)
-                data.items.forEach(item => {
-                    video = `
+});
+
+
+}
+
+
+
+    $(document).ready(function () {
+        var youtubekey = 'AIzaSyDGxdfjPLDMkjD0Cvi9dU8d66Pv1SlJ08k'
+        var video = ''
+        $('#form').on('click', function (event) {
+            event.preventDefault()
+            console.log('clicked')
+            var search = $('#search').val()
+            videoSearch(youtubekey, search, 5)
+            console.log(search.value)
+        })
+        function videoSearch(key, search, maxResults) {
+            $('#videos').empty()
+            $.get('https://www.googleapis.com/youtube/v3/search?key=' + key +
+                '&type=video&part=snippet&maxResults=' + maxResults + '&q=' + search, function (data) {
+                    console.log(data)
+                    data.items.forEach(item => {
+                        video = `
                 <iframe class="has-ratio" width="360" height="360" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" 
                 allowfullscreen></iframe>
                 `
-                    $('#videos').append(video)
-                });
-            })
-    }
-})
+                        $('#videos').append(video)
+                    });
+                })
+        }
+    })
 
