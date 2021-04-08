b9963398af84d453075d7bd9d666e34dbea6c876
Mattjb97 <bohlmatthew@gmail.com>
2021-04-08 13:04:46 -0500

    code refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,18 @@
 var searchbutton = document.querySelector('.is-info')
 var input = document.querySelector('#search')
-
 var videoLocation = document.querySelector('.boxVideo')
 var lyricsLocation = document.querySelector('.boxLyrics')
 
 
-
-//searches for the song to get lyrics 
-
-
+// when clicking the search button the result is then taken and given a variable 
 $('#form').on('click', function (event) {
     event.preventDefault()
     var search = $('#search').val()
     getSongs(search)
+    //removes old lyrics and clears search bar on the click
     lyricsLocation.innerHTML = "";
 })
-
+//takes value on the input variable and places it into the URL of the musixmatch API 
 function getSongs(songTitle) {
     var apiUrl = 'https://api.musixmatch.com/ws/1.1/track.search?q_track=' + songTitle + '&page_size=5&s_track_rating=desc&apikey=6a4d09aa7c7bc21dd8f981caaf324cda';
     fetch(apiUrl).then(function (response) {
@@ -25,7 +22,6 @@ function getSongs(songTitle) {
                 var songList = currentData.message.body.track_list;
                 console.log(songList);
                 displayList(songList);
-                // return songList;
             });
         } else {
             alert('Error: ' + response.statusText);
@@ -36,45 +32,33 @@ function getSongs(songTitle) {
         });
 };
 
+// once the songs are retreved this dynamically produces buttons into the 'Song Choices and Lyrics' box for the top five results  
 function displayList(songArray) {
     var appendEl = document.querySelector(".list-group");
     appendEl.innerHTML = "";
     songArray.forEach(song => {
         var liEl = document.createElement("button")
-
+        //give the buttons a click function that runs the apiFX function
         liEl.addEventListener("click", function () { apiFX(song.track.track_id, song.track.artist_name, song.track.track_name) })
-
         liEl.textContent = "Artist: " + song.track.artist_name + " - Song: " + song.track.track_name;
-
         liEl.classList.add("list-group-item", "songbuttons");
-
         appendEl.appendChild(liEl)
     })
-
-
 }
 
 
-
+//the buttons have data about each song from the API call this function takes the data from the previous call and puts it back into the API to then generate the lyrics into a new box 
 function apiFX(songID, songArtist, songName) {
     var apiUrl2 = 'https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + songID + '&apikey=6a4d09aa7c7bc21dd8f981caaf324cda';
-
     console.log(apiUrl2);
-
     fetch(apiUrl2).then(function (response) {
         if (response.ok) {
             response.json().then(data => {
                 console.log(lyrics);
                 console.log(songArtist);
                 console.log(songName);
-                // var songList=currentData.message.body.track_list;
-                // console.log(songList);
-                // displayList(songList);
-
                 var lyricsPop = data['message']['body']['lyrics']['lyrics_body'];
-
                 lyricsLocation.innerHTML = lyricsPop;
-
             });
         } else {
             alert('Error: ' + response.statusText);
@@ -83,23 +67,26 @@ function apiFX(songID, songArtist, songName) {
         .catch(function (error) {
             alert('Unable to connect to MusiXmatch');
         });
-
 }
 
+//adds the previous serches to local storage 
+function storeSong() {
+    localStorage.setItem("searchSong", JSON.stringify(songSearches));
+    return;
+};
 
-
-
-// youtube video function 
-
-var youtubekey = 'AIzaSyDGxdfjPLDMkjD0Cvi9dU8d66Pv1SlJ08k'
+//sets variables for youtubes api key and the search input on click
+var youtubekey = 'AIzaSyClOnNDd4howxJo-Q-1PXhG2Y__Jo44jP4'
 var video = ''
 $('#form').on('click', function (event) {
     event.preventDefault()
     console.log('clicked')
     var search = $('#search').val()
+    //declares variables for the videoSearch function
     videoSearch(youtubekey, search, 5)
     console.log(search.value)
 })
+//takes the variables runs them through the youtube API and then dynamically generates the videos on the right
 function videoSearch(key, search, maxResults) {
     $('#videos').empty()
     $.get('https://www.googleapis.com/youtube/v3/search?key=' + key +
@@ -114,3 +101,4 @@ function videoSearch(key, search, maxResults) {
             });
         })
 }
+
